0dcb9b3ecc2e3cd4d9096330a5dfea1e87e35c1d
abrantel <acbrante@gmail.com>
2021-09-09 18:02:27 +0000

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+
+import Header from "./components/Header.js";
+import Game from "./components/Game.js"
+import "./style.css"
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Game />
+    </div>
+  );
+}
+
+export default App;
